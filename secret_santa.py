60fb6ce4cfcb7d469ff01ea34d0cae321a049567
lorenanda <lorena.ciutacu@gmail.com>
2020-12-25 13:30:07 +0100

    update secret_santa emailing

--- secret_santa.py
+++ secret_santa.py
@@ -5,55 +5,54 @@ import numpy as np
 import datetime
 import pandas as pd
 import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 dt = datetime.datetime.now()
 date = dt.strftime("%Y-%m-%d at %I:%M%p")
 
+friends = {"Friend 1": "friend1@mail.com", "Friend 2": "friend2@mail.com"}
 
-def pair_people():
-    friends = {
-        "Ahmad": "ahmad@mail.com",
-        "Bianca": "bianca@mail.com",
-        "Chad": "chad@mail.com",
-        "Damian": "damian@mail.com",
-        "Ellen": "ellen@mail.com",
-        "Franz": "franz@mail.com",
-    }
+
+def notify_secret_santa():
+    givers_email = list(friends.values())
 
     givers = list(
         np.random.choice(list(friends.keys()), len(list(friends.keys())), replace=False)
     )
+
     receivers = []
     for i in range(-1, len(givers) - 1):
         receivers.append(givers[i])
-    print(givers, receivers)
 
-    secret_santa_df = pd.DataFrame({"givers": givers, "receivers": receivers})
-    secret_santa_df.to_csv("secret_givers_list.csv", index=False)
+    secret_santa_df = pd.DataFrame(
+        {"givers_email": givers_email, "givers": givers, "receivers": receivers}
+    )
+    secret_santa_df
 
+    my_email = ""
+    my_pw = ""
+    subject = ""
 
-def send_email():
-    smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
-    smtpObj.ehlo()
-    smtpObj.starttls()
-    your_email = input("Enter your email: ")
-    your_password = input("Enter your password: ")
-    smtpObj.login(your_email, your_password)
+    smtp_host = "smtp.gmx.com"  # smtp.gmail.com / smtp.yahoo.com / smtp.live.com
 
-    for i in range(len(friends)):
-        smtpObj.sendmail(
-            your_email,
-            friends[givers[i]],
-            "Subject: Secret Santa Pairs \
-        \nHello %s! \
-        \n\nThis year you are getting a present for %s! \
-        \n\nEmail sent on %s."
-            % (givers[i], receivers[i], date),
-        )
+    s = smtplib.SMTP(smtp_host, 587, timeout=10)  # or port 465
+    # s.set_debuglevel(1)
 
-    smtpObj.quit()
+    for i in range(len(secret_santa_df)):
+        s.starttls()
+        s.login(my_email, my_pw)
+
+        body = str("Hi, " + givers[i] + "!\n\nYou get a present for " + receivers[i])
+        msg = MIMEText(body, "plain", "utf-8")
+        msg["subject"] = Header(subject, "utf-8")
+        msg["From"] = my_email
+        msg["To"] = givers_email[i]
+
+        s.sendmail(msg["From"], msg["To"], msg.as_string())
+        print("Email sent!")
+        s.quit()
 
 
 if __name__ == "__main__":
-    pair_people()
-    send_email()
+    notify_secret_santa()
