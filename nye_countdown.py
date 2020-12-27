e6276f17ffdfe3385f631d5634cd24787ec6cad9
lorenanda <lorena.ciutacu@gmail.com>
2020-12-27 20:24:36 +0100

    add 2021 datetime to nye_countdown

--- nye_countdown.py
+++ nye_countdown.py
@@ -1,23 +1,43 @@
-import os
 import time
+import datetime
 import pyttsx3
 
 
-def countdown(t):
+def countdown(target_datetime):
+    while True:
+        difference = target_datetime - datetime.datetime.now()
+        count_hours, rem = divmod(difference.seconds, 3600)
+        count_minutes, count_seconds = divmod(rem, 60)
 
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = "{:02d}:{:02d}".format(mins, secs)
-        print(timer, end="\r")
+        if (
+            difference.days == 0
+            and count_hours == 0
+            and count_minutes == 0
+            and count_seconds == 0
+        ):
+            engine = pyttsx3.init()
+            engine.say("Happy New Year! Have fun breaking your resolutions.")
+            engine.runAndWait()
+
+            print("Happy New Year!")
+            break
+
+        else:
+            print(
+                "Countdown to New Year: "
+                + str(difference.days)
+                + " day(s) "
+                + str(count_hours)
+                + " hour(s) "
+                + str(count_minutes)
+                + " minute(s) "
+                + str(count_seconds)
+                + " second(s) "
+            )
+
         time.sleep(1)
-        t -= 1
-
-    engine = pyttsx3.init()
-    engine.say("Happy New Year! Have fun breaking your resolutions.")
-    engine.runAndWait()
-
-    print("Happy New Year!")
 
 
 if __name__ == "__main__":
-    countdown(3)
+    end_time = datetime.datetime(2021, 1, 1, 00, 00)
+    countdown(end_time)
