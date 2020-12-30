cd0163ed4603a0a446c9d70306ec286a39dd2dc3
lorenanda <lorena.ciutacu@gmail.com>
2020-12-30 17:34:07 +0100

    start resolutions list app

---
+++ resolutions_list.py
@@ -0,0 +1,8 @@
+"""This program creates a GUI list for New Year resolutions."""
+
+from tkinter import *
+from tkinter.font import Font
+
+root = Tk()
+root.title("New Year Resolutions")
+root.geometry("500x500")
